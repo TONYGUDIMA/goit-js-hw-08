c61a2867df6133724aa36d7ded3a14f8df85793a
TONYGUDIMA <gudima.anton@yahoo.com>
2023-03-08 22:19:15 +0200

    fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+function createGallery(items) {
+  return items
+  .map( item => `
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>`)
+  .join('')
+}
+
+const gallery = document.querySelector('.gallery')
+gallery.innerHTML = createGallery(galleryItems)
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData : `alt`, captionDelay : 250, }); 
